8c498ce9403be6d8b71f54229a3052fe7a5a038c
Shweta Chavan <chavan.shweta@gmail.com>
2020-02-26 10:26:38 -0500

    Update get_num_clusters_by_ccf.R

--- get_num_clusters_by_ccf.R
+++ get_num_clusters_by_ccf.R
@@ -8,33 +8,30 @@ sampleid = c(
              ,'s_C_006799_P001_d','s_C_006757_P002_d','s_C_000796_P001_d','s_C_001566_M001_d','s_C_006846_M001_d','s_C_001699_P001_d')
 length(sampleid)
 for(i in 1:length(sampleid)){
-    par(mfrow=c(3,1))
+  par(mfrow=c(3,1))
+  set.seed(100)
+  #wes data
+  data = NULL
+  data = filter(wesmaf, Tumor_Sample_Barcode == sampleid[i]) %>% 
+    select(ccf_expected_copies_em) %>% filter(ccf_expected_copies_em >=0) %>%
+    mutate_all( ~replace(., is.na(.), 0))
+  dim(data)
+  nmut = dim(data)[1]
+  p1 = plot(data, main = sampleid[i], xlab = "CCF")
   
-    #wes
-    set.seed(100)
-    data = NULL
-    data = filter(wesmaf, Tumor_Sample_Barcode == sampleid[i]) %>% 
-              select(ccf_expected_copies_em) %>% filter(ccf_expected_copies_em >=0) %>%
-              mutate_all( ~replace(., is.na(.), 0))
-    dim(data)
-    nmut = dim(data)[1]
-    p1 = plot(data, main = sampleid[i], xlab = "CCF")
-    
-    density_data = NULL
-    density_data = density(data$ccf_expected_copies_em)
-    p2 = plot(density_data, main = sampleid[i])
+  density_data = NULL
+  density_data = density(data$ccf_expected_copies_em)
+  p2 = plot(density_data, main = sampleid[i])
   
-    formatted_data = NULL  
-    formatted_data = as.data.frame(cbind(density_data$x, density_data$y)); names(formatted_data) = c('CCF','Density')
-    head(formatted_data)
-    gap = NULL #
-    gap <- clusGap(formatted_data, kmeans, K.max=ceiling(nmut/10), B=500)
-    k = NULL
-    k <- maxSE(gap$Tab[, "gap"], gap$Tab[, "SE.sim"], method="Tibs2001SEmax") #Tibs2001SEmax #globalSEmax #firstSEmax #firstmax #globalmax
-    print(paste(sampleid[i],"nmut=",nmut,"clusters=",k,"...Done!\n")) #2
-    p3 = plot(gap, main = paste(sampleid[i],"predicted clusters=",k))
-
-    p1
-    p2
-    p3
+  formatted_data = NULL  
+  formatted_data = as.data.frame(cbind(density_data$x, density_data$y)); names(formatted_data) = c('CCF','Density')
+  head(formatted_data)
+  
+  gap = NULL; k = NULL
+  gap <- clusGap(formatted_data, kmeans, K.max=ceiling(nmut/10), B=500)
+  k <- maxSE(gap$Tab[, "gap"], gap$Tab[, "SE.sim"], method="Tibs2001SEmax") #Tibs2001SEmax #globalSEmax #firstSEmax #firstmax #globalmax
+  p3 = plot(gap, main = paste(sampleid[i],"predicted clusters=",k))
+  
+  print(paste(sampleid[i],"nmut=",nmut,"clusters=",k,"...Done!\n"))
+  p1; p2; p3
 }
